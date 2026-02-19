0509f558ef5faa030d608188a7c301cf6e193f95
FatPandaC8 <bachcodethings@gmail.com>
2026-02-19 14:43:16 +0700

    Done: the auth page, next is the home page

--- frontend/app/pages/auth.vue
+++ frontend/app/pages/auth.vue
@@ -4,9 +4,10 @@ import * as z from 'zod'
 import type { FormSubmitEvent } from '@nuxt/ui'
 
 const activeTab = ref('signin')
-const rememberMe = ref(false)
+const rememberMe = ref(true)
 const showPassword = ref(false)
 const showConfirmPassword = ref(false)
+const agreeTerms = ref(false)
 
 const items = [
   { label: 'Sign In', value: 'signin', slot: 'signin' },
@@ -30,7 +31,7 @@ const signUpState = reactive({
 })
 
 const signInSchema = z.object({
-  email: z.string().email('Invalid email'),
+  email: z.email('Invalid email format'),
   password: z.string().min(8, 'Must be at least 8 characters')
 })
 
@@ -38,8 +39,8 @@ const signUpSchema = z.object({
   name: z.string().min(2, 'Name required'),
   email: z.email('Invalid email'),
   password: z.string().min(8, 'Must be at least 8 characters'),
-  confirmPassword: z.string(),
-  role: z.string().min(1)
+  confirmPassword: z.string().min(8, 'Password does not match'),
+  role: z.string().min(1, 'Please select a role.')
 }).refine(data => data.password === data.confirmPassword, {
   message: 'Passwords must match',
   path: ['confirmPassword']
@@ -61,7 +62,7 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
             <!--Breadcrumb-->
             <div class="flex flex-1 mb-10 items-center">
                 <UIcon name="i-lucide-arrow-left"/>
-                <NuxtLink to="#" class="text-gray-500 ml-2">
+                <NuxtLink to="#" class="text-gray-500 ml-2 hover:text-gray-400">
                     Back to Home
                 </NuxtLink>
             </div>
@@ -107,7 +108,7 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
                 <!--Info-->
         
                 <!--Auth-->
-                <UCard class="w-full max-w-lg border-blue-100 bg-white/90 backdrop-blur-sm shadow-2xl shadow-blue-500/10 p-9 rounded-2xl">
+                <UCard class="w-full max-w-lg border-blue-100 bg-white/90 backdrop-blur-sm shadow-2xl shadow-blue-500/10 p-9 rounded-2xl flex flex-col">
                     <UTabs 
                     v-model="activeTab"
                     :items="items" 
@@ -131,38 +132,42 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
                                 </div>
     
                                 <UForm class="space-y-4" :state="signInState" :schema="signInSchema" @submit="onSignIn">
-                                    <UFormField name="email" label="Email">
+                                    <UFormField name="email" label="Email" :ui="{
+                                        error: 'text-red-500 italic bg-red-500/15 text-center rounded h-6 p-1',
+                                    }">
                                         <UInput 
                                             v-model="signInState.email"
                                             type="email"
                                             placeholder="example@example.com"
                                             :ui="{
-                                                base: 'border border-blue-100 focus:border-blue-300 px-2 h-10'
+                                                base: 'border border-blue-300 focus:border-blue-600 px-2 h-10 rounded',
                                             }"
                                             
                                         />
                                     </UFormField>
     
-                                    <UFormField name="password" label="Password">
+                                    <UFormField name="password" label="Password" :ui="{
+                                        error: 'text-red-500 italic bg-red-500/15 text-center rounded h-6 p-1',
+                                    }">
                                         <UInput
                                             v-model="signInState.password"
                                             type="password"
                                             placeholder="example123"
                                             :ui="{
-                                                base: 'border border-blue-100 focus:border-blue-300 px-2 h-10'
+                                                base: 'border border-blue-300 focus:border-blue-600 px-2 h-10 rounded'
                                             }"
                                         />
                                     </UFormField>
     
                                     <div class="flex items-center justify-between">
-                                        <div class="flex items-center space-x-2">
-                                        <UCheckbox
-                                            v-model="rememberMe"
-                                            class="text-sm cursor-pointer"
-                                            label="Remember me"
-                                        />
-                                        
-                                        </div>
+                                        <label class="flex items-center cursor-pointer">
+                                            <UCheckbox
+                                                v-model="rememberMe"
+                                                class="w-4 h-4 shrink-0 border border-blue-500 rounded mr-2 justify-center"
+                                            />
+                                            <span class="text-sm">Remember me</span>
+                                        </label>
+
                                         <NuxtLink
                                             to="#"
                                             class="text-sm text-blue-600 hover:text-blue-700 font-medium"
@@ -174,7 +179,7 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
                                     <UButton 
                                         type="submit"
                                         block 
-                                        class="bg-linear-to-r from-blue-600 to-blue-500 hover:from-blue-700 hover:to-blue-600 shadow-lg shadow-blue-500/30 text-white cursor-pointer h-8"
+                                        class="bg-linear-to-r from-blue-600 to-blue-500 hover:from-blue-700 hover:to-blue-600 shadow-lg shadow-blue-500/30 text-white cursor-pointer h-8 rounded-xl"
                                         size="lg">
                                         Sign In
                                     </UButton>
@@ -192,7 +197,7 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
                                         <UButton
                                         variant="outline"
                                         block 
-                                        class="border-blue-200 bg-blue-50 hover:bg-blue-100 hover:text-blue-500 hover:cursor-pointer h-8"
+                                        class="border border-blue-200 bg-blue-50 hover:bg-blue-100 hover:text-blue-500 hover:cursor-pointer h-8 rounded-xl"
                                         icon="i-logos-google-icon"
                                         >
                                             Google
@@ -207,66 +212,69 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
                                 <div class="text-center mb-6 mt-8">
                                     <h3 class="text-2xl font-bold mb-2">Create Account</h3>
                                     <p class="text-sm text-gray-500">
-                                    Join thousands of professionals on ExpoVerse
+                                        Join thousands of professionals on ExpoVerse
                                     </p>
                                 </div>
     
-                                <UForm :schema="signUpSchema" :state="signUpState" @submit="" class="space-y-4">
-                                    <UFormField name="name" label="Full Name">
+                                <UForm :schema="signUpSchema" :state="signUpState" @submit="onSignUp" class="space-y-4">
+                                    <UFormField name="name" label="Full Name" :ui="{
+                                        error: 'text-red-500 italic bg-red-500/15 text-center rounded h-6 p-1',
+                                    }">
                                         <UInput
                                             v-model="signUpState.name"
                                             placeholder="John Doe"
                                             :ui="{
-                                                base: 'border border-blue-500 focus:border-blue-300 px-2 h-10'
+                                                base: 'border border-blue-300 focus:border-blue-600 px-2 h-10 rounded'
                                             }"
                                         />
                                     </UFormField>
     
-                                    <UFormField name="email" label="Email">
+                                    <UFormField name="email" label="Email" :ui="{
+                                        error: 'text-red-500 italic bg-red-500/15 text-center rounded h-6 p-1',
+                                    }">
                                         <UInput
                                             v-model="signUpState.email"
                                             type="email"
                                             placeholder="your.email@example.com"
                                             :ui="{
-                                                base: 'border border-blue-100 focus:border-blue-300 px-2 h-10'
+                                                base: 'border border-blue-300 focus:border-blue-600 px-2 h-10 rounded'
                                             }"
                                         />
                                     </UFormField>
     
-                                    <UFormField name="password" label="Password">
+                                    <UFormField name="password" label="Password" :ui="{
+                                        error: 'text-red-500 italic bg-red-500/15 text-center rounded h-6 p-1',
+                                    }">
                                         <UInput
                                             v-model="signUpState.password"
                                             :type="showPassword ? 'text' : 'password'"
                                             placeholder="Create a strong password"
                                             :ui="{
-                                                base: 'border border-blue-100 focus:border-blue-300 px-2 h-10'
+                                                base: 'border border-blue-300 focus:border-blue-600 px-2 h-10 rounded'
                                             }"
                                         />
                                     </UFormField>
     
-                                    <UFormField name="confirmPassword" label="Confirm Password">
+                                    <UFormField name="confirmPassword" label="Confirm Password" :ui="{
+                                        error: 'text-red-500 italic bg-red-500/15 text-center rounded h-6 p-1',
+                                    }">
                                         <UInput
                                             v-model="signUpState.confirmPassword"
                                             :type="showConfirmPassword ? 'text' : 'password'"
                                             placeholder="Re-enter your password"
                                             :ui="{
-                                                base: 'border border-blue-100 focus:border-blue-300 px-2 h-10'
+                                                base: 'border border-blue-300 focus:border-blue-600 px-2 h-10 rounded'
                                             }"
                                         >
-                                            <template #trailing>
-                                                <UButton
-                                                    variant="ghost"
-                                                    size="xs"
-                                                    @click="showConfirmPassword = !showConfirmPassword"
-                                                >
-                                                </UButton>
-                                            </template>
                                         </UInput>
                                     </UFormField>
     
-                                    <UFormField name="role" label="Role">
+                                    <UFormField name="role" label="Role" :ui="{
+                                        error: 'text-red-500 italic bg-red-500/15 text-center rounded h-6 p-1',
+                                    }">
                                         <USelect
                                             v-model="signUpState.role"
+                                            variant="soft"
                                             :items="roles"
                                             placeholder="Select your role"
                                             :content="{
@@ -274,29 +282,41 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
                                                 side: 'bottom',
                                             }"
                                             :ui="{
-                                                base: 'border border-blue-100 w-full h-10'
+                                                base: 'w-full border border-blue-300 h-10 rounded bg-blue-50 ',
+                                                content: 'bg-white rounded-xl shadow-lg border border-blue-100',
+                                                item: 'px-3 py-2 text-sm hover:bg-blue-50 cursor-pointer w-100 rounded-xl',
+                                                itemLabel: 'text-gray-700',
+                                                itemTrailingIcon: 'text-blue-500',
                                             }"
+                                            trailing-icon="null"
+                                            
                                         />
                                     </UFormField>
     
                                     <div class="flex items-start gap-2">
-                                    <UCheckbox required />
-    
-                                    <p class="text-sm text-gray-600">
-                                        I agree to the
-                                        <NuxtLink
-                                            to="#"
-                                            class="text-blue-600 font-medium"
-                                        >
-                                        Terms of Service
-                                        </NuxtLink>
-                                    </p>
+                                        <p class="text-sm text-gray-600">
+                                            <label class="flex items-center cursor-pointer">
+                                                <UCheckbox
+                                                    v-model="agreeTerms"
+                                                    class="w-4 h-4 shrink-0 border border-blue-500 rounded mr-2 justify-center"
+                                                    required
+                                                />
+                                                <span class="text-sm">I agree to the
+                                                    <NuxtLink
+                                                        to="#"
+                                                        class="text-blue-600 font-medium"
+                                                    >
+                                                    Terms of Service
+                                                    </NuxtLink>
+                                                </span>
+                                            </label>
+                                        </p>
                                     </div>
     
                                     <UButton 
                                         type="submit"
                                         block 
-                                        class="bg-linear-to-r from-blue-600 to-blue-500 hover:from-blue-700 hover:to-blue-600 shadow-lg shadow-blue-500/30 text-white cursor-pointer h-8"
+                                        class="bg-linear-to-r from-blue-600 to-blue-500 hover:from-blue-700 hover:to-blue-600 shadow-lg shadow-blue-500/30 text-white cursor-pointer h-8 rounded-xl"
                                         size="lg">
                                         Sign Up
                                     </UButton>
@@ -314,7 +334,7 @@ function onSignUp(event: FormSubmitEvent<typeof signUpState>) {
                                         <UButton
                                         variant="outline"
                                         block 
-                                        class="border-blue-200 bg-blue-50 hover:bg-blue-100 hover:text-blue-500 hover:cursor-pointer h-8"
+                                        class="border border-blue-200 bg-blue-50 hover:bg-blue-100 hover:text-blue-500 hover:cursor-pointer h-8 rounded-xl"
                                         icon="i-logos-google-icon"
                                         >
                                             Google
